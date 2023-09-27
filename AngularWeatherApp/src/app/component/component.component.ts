bce256375c5439a5527f6f52b7b46c6b66bc3597
dotnet-code <@dotnet-code>
2023-09-27 13:03:33 +0000

    commit from code202309270333

---
+++ AngularWeatherApp/src/app/component/component.component.ts
@@ -0,0 +1,39 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-component',
+  templateUrl: './component.component.html',
+  styleUrls: ['./component.component.css']
+})
+export class ComponentComponent implements OnInit {
+  myForm: FormGroup;
+  showAddRepositoryPopup: boolean = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.myForm = new FormGroup({
+      jiraConnection: new FormControl('', Validators.required),
+      jiraProject: new FormControl('', Validators.required),
+      implementationType: new FormControl('', Validators.required),
+      github: new FormControl('', Validators.required),
+      githubRepository: new FormControl('', Validators.required),
+      newRepositoryName: new FormControl(''),
+      packageName: new FormControl('', Validators.required),
+      userStories: new FormControl('', Validators.required)
+    });
+  }
+
+  openAddRepositoryPopup(): void {
+    this.showAddRepositoryPopup = true;
+  }
+
+  saveNewRepository(): void {
+    // Save new repository logic
+  }
+
+  generateAngularCode(): void {
+    // Generate Angular code logic
+  }
+}
